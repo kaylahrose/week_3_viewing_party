3ff778aec6c3e535f4fa453d08c8b84aab950b28
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-02-13 10:10:14 -0800

    Add authentication with user password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
-class User <ApplicationRecord 
-    validates_presence_of :email, :name 
-    validates_uniqueness_of :email
+class User < ApplicationRecord
+  include BCrypt
+  validates_presence_of :email, :name
+  validates_uniqueness_of :email
+  validates_presence_of :password_digest
+  has_secure_password
 
-end 
+#   def password
+#     @password ||= Password.new(password_digest)
+#   end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_digest = @password
+  end
+end
--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -1,28 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe "User Registration" do
+RSpec.describe 'User Registration' do
   it 'can create a user with a name and unique email' do
     visit register_path
 
     fill_in :user_name, with: 'User One'
-    fill_in :user_email, with:'user1@example.com'
+    fill_in :user_email, with: 'user1@example.com'
+    fill_in :user_password, with: 'password'
     click_button 'Create New User'
 
     expect(current_path).to eq(user_path(User.last.id))
     expect(page).to have_content("User One's Dashboard")
-  end 
+  end
 
-  it 'does not create a user if email isnt unique' do 
-    User.create(name: 'User One', email: 'notunique@example.com')
+  it 'does not create a user if email isnt unique' do
+    User.create(name: 'User One', email: 'notunique@example.com', password: 'password')
 
     visit register_path
-    
+
     fill_in :user_name, with: 'User Two'
-    fill_in :user_email, with:'notunique@example.com'
+    fill_in :user_email, with: 'notunique@example.com'
+    fill_in :user_password, with: 'password'
     click_button 'Create New User'
 
     expect(current_path).to eq(register_path)
-    expect(page).to have_content("Email has already been taken")
-    
+    expect(page).to have_content('Email has already been taken')
   end
 end
